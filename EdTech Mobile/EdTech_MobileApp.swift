0af01c2e4ecb422e141246ffccf02e9351ec5dac
Vishavesh Tyagi <vishavesh.tyagi@citrix.com>
2023-09-09 18:08:17 +0530

    first commit

---
+++ EdTech Mobile/EdTech_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdTech_MobileApp.swift
+//  EdTech Mobile
+//
+//  Created by Vishavesh on 08/09/23.
+//
+
+import SwiftUI
+
+@main
+struct EdTech_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
